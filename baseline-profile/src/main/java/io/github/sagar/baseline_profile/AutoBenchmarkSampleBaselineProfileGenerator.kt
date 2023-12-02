5d0c6bb9cf6c70771fd2de167feb00798c9fde2f
Sagar Viradiya <sagarviradiya4444@gmail.com>
2023-12-02 18:44:18 +0530

    Update dependency
    - Update macro-benchmark dependency

--- baseline-profile/src/main/java/io/github/sagar/baseline_profile/AutoBenchmarkSampleBaselineProfileGenerator.kt
+++ baseline-profile/src/main/java/io/github/sagar/baseline_profile/AutoBenchmarkSampleBaselineProfileGenerator.kt
@@ -24,19 +24,17 @@
 
 package io.github.sagar.baseline_profile
 
-import androidx.benchmark.macro.ExperimentalBaselineProfilesApi
 import androidx.benchmark.macro.junit4.BaselineProfileRule
 import org.junit.Rule
 import org.junit.Test
 
-@OptIn(ExperimentalBaselineProfilesApi::class)
 class AutoBenchmarkSampleBaselineProfileGenerator {
 
     @get:Rule
     val baselineProfileRule = BaselineProfileRule()
 
     @Test
-    fun startup() = baselineProfileRule.collectBaselineProfile(
+    fun startup(): Unit = baselineProfileRule.collect(
         packageName = "io.github.sagar.auto_benchmark",
         profileBlock = {
             startActivityAndWait()
